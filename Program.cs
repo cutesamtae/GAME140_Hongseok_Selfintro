7c8cf5c7cfe74da31c7aa0627e9f9cb4a472f9af
cutesamtae <cutehongseok2007@gmail.com>
2026-08-28 16:08:58 +0900

    replace something

--- Program.cs
+++ Program.cs
@@ -1,19 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-
 const string name = "Hongseok Kim";
-const string information = "I already learned Data Structures in C language";
+const string interestingFact = "I already learned Data Structures in C language";
 const string book = "Metropolis";
 int year = 2007;
-DateTime date = DateTime.Now;
+//DateTime date = DateTime.Now;
 
-//print name
+//print myname
 Console.WriteLine($"Hello, My name is {name}.");
 
 //print born year
 Console.WriteLine("I was born in " + year + ".");
 
 //print personal information
-Console.WriteLine($"An Interesting Fact About Me is {information}.\nMy favorite book is {book}.");
+Console.WriteLine($"An Interesting Fact About Me is {interestingFact}.\nMy favorite book is \"{book}\".");
 
 //print date
-Console.WriteLine($"Today is {date.Date}.");
+Console.WriteLine($"Today is {DateTime.Now}.");
